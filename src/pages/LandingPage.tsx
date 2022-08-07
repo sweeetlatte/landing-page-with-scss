4ebc1759175c3e08fe2707b49fa0f31a7ca2ae22
sweetcapucino <thuylinh271055@gmail.com>
2022-08-07 16:08:31 +0700

    style

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,3 +1,5 @@
+import "./landingPage.scss";
+
 import AboutUs from "../components/LandingPage/AboutUs/AboutUs";
 import Footer from "../components/LandingPage/Footer/Footer";
 import Form from "../components/LandingPage/Form/Form";
@@ -7,7 +9,7 @@ import Testimonial from "../components/LandingPage/Testimonial/Testimonial";
 
 export default function LandingPage() {
     return (
-        <div>
+        <div className="landing-page">
             <Hero />
             <AboutUs />
             <Project />
