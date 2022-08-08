4d9b3b719f0f5db9d828555e0fadb4a7774fb0c5
sweetcapucino <thuylinh271055@gmail.com>
2022-08-08 18:20:55 +0700

    feat: testimonial

---
+++ src/components/LandingPage/Testimonial/Testimonial.tsx
@@ -0,0 +1,12 @@
+import Slider from "../../Slider/Slider";
+import "./testimonial.scss";
+import Texts from "./Texts";
+
+export default function Testimonial() {
+    return (
+        <div className="testimonial">
+            <Texts />
+            <Slider />
+        </div>
+    );
+}
---
+++ src/components/LandingPage/Testimonial/Texts.tsx
@@ -0,0 +1,10 @@
+import "./testimonial.scss";
+
+export default function Texts() {
+    return (
+        <div className="testimonial-texts">
+            <div>Few words</div>
+            <div>Let’s have a look what our clients are saying about us</div>
+        </div>
+    );
+}
